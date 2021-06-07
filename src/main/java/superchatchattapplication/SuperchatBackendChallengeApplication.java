b9ba233f239065c23fdf146ebd710c1e8dcebe93
alemlishar <alextopten03@gmail.com>
2021-06-07 16:22:02 +0200

    start the project, imoport the springboot initilizer

---
+++ src/main/java/superchatchattapplication/SuperchatBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package superchatchattapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperchatBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperchatBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/superchatchattapplication/SuperchatBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package superchatchattapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperchatBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
