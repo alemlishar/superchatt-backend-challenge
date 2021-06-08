4ca292e68f65050795a9f388186031795d7ee3d4
alemlishar <alextopten03@gmail.com>
2021-06-08 09:41:53 +0200

    Rest endpoint controler first commit

--- src/main/java/challenge_superchat/dk/chattcontroller/ChattRestController.java
+++ src/main/java/challenge_superchat/dk/chattcontroller/ChattRestController.java
@@ -1,5 +1,37 @@
 package challenge_superchat.dk.chattcontroller;
 
+import java.util.Set;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import challenge_superchat.dk.chattmodel.Conversations;
+import challenge_superchat.dk.chattmodel.Message;
+import challenge_superchat.dk.chattmodel.User;
+
+
+@RestController
 public class ChattRestController {
 
+	
+	@PostMapping("/contacts")
+    public  Set<User> createContacts() {
+        return null;
+    }
+
+    @GetMapping("/contacts")
+    public boolean listContacts(Message message) {
+        return true;
+    }
+
+	@PostMapping("/message")
+    public  boolean sendMessage() {
+        return true;
+    }
+	
+	@PostMapping("/conversations")
+    public  Set<Conversations> listConversations() {
+        return null;
+    }
 }
